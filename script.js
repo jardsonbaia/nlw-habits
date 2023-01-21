c3d3e7ee27b8266378d1faf43b1a05a86cbf90b3
jardsonbaia <jardsonbaia@hotmail.com>
2023-01-20 21:19:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form-habits')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    
+    const today = new Date().toLocaleString('pt-br').slice(0, 5)
+
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert("❌ O dia já foi adicionado!")
+        return
+    } 
+    
+    nlwSetup.addDay(today)
+}
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
+
